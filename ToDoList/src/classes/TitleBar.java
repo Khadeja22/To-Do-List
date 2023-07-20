08f4c445aeec5324ea33d281201a14ee69218417
Khadeja22 <112954675+Khadeja22@users.noreply.github.com>
2023-07-20 01:59:50 -0400

    Project upload

---
+++ ToDoList/src/classes/TitleBar.java
@@ -0,0 +1,27 @@
+package classes;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class TitleBar extends JPanel{
+	
+	public static final String BorderLayout = null;
+
+	//Constructor
+	TitleBar(){
+		this.setPreferredSize(new Dimension(400, 80));
+		
+		JLabel titleText = new JLabel("To Do List <3");
+		titleText.setPreferredSize(new Dimension(200, 80));
+		titleText.setFont(new Font("Monospaced", Font.BOLD, 20));
+		titleText.setHorizontalAlignment(JLabel.CENTER);
+		
+		this.add(titleText);
+		
+	}
+
+}
